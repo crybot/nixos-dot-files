2b43b9974ee112cc4be762f0b48e8acc152bbd43
Marco <crybot@hotmail.it>
2025-03-12 21:32:25 +0100

    Update configuration.nix

--- nixos-config/configuration.nix
+++ nixos-config/configuration.nix
@@ -59,7 +59,7 @@ in
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
-  i19n.extraLocaleSettings = {
+  i18n.extraLocaleSettings = {
     LC_ADDRESS = "it_IT.UTF-8";
     LC_IDENTIFICATION = "it_IT.UTF-8";
     LC_MEASUREMENT = "it_IT.UTF-8";
