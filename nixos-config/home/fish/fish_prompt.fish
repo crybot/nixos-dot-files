90cdc9488a9ab00d3609ab73143df414bdeb9e9f
crybot <crybot@hotmail.it>
2025-06-02 19:48:39 +0200

    Show ssh info in fish prompt

--- nixos-config/home/fish/fish_prompt.fish
+++ nixos-config/home/fish/fish_prompt.fish
@@ -27,17 +27,30 @@ if test $last_status -ne 0
 end
 
 # # Add virtualenv display
+set -l venv_prompt ''
 if set -q VIRTUAL_ENV; and not functions -q deactivate
   # Extract the name (basename) of the virtual environment directory
   set -l venv_name (basename "$VIRTUAL_ENV")
   # Output the name, maybe with formatting (e.g., parentheses, color)
   # This example uses blue color
-  echo -n -s (set_color blue) "(" $venv_name ")" (set_color normal) " "
+  # echo -n -s (set_color blue) "(" $venv_name ")" (set_color normal) " "
+  set venv_prompt (set_color blue) "(" $venv_name ")" (set_color normal) " "
 end   
 
+# SSH connection indicator
+set -l ssh_indicator ''
+if set -q SSH_CONNECTION
+  # SSH_CONNECTION format: client_ip client_port server_ip server_port
+  set -l ssh_parts (string split ' ' $SSH_CONNECTION)
+  set -l server_ip $ssh_parts[3] # The server IP is the 3rd element
+  set ssh_indicator (set_color yellow) "[ssh to $server_ip] " (set_color normal)
+end
+
 # Show virtualenv/mode prompt (relies on fish_mode_prompt checking VIRTUAL_ENV)
-printf '%s' (fish_mode_prompt)
+set -l mode_prompt ''
+if functions -q fish_mode_prompt # Check if the function exists
+  set mode_prompt (fish_mode_prompt)
+end
 
-
-echo -s (prompt_login) ' ' $cwd_color (prompt_pwd) $normal $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
+echo -s $ssh_indicator $mode_prompt $venv_prompt (prompt_login) ' ' $cwd_color (prompt_pwd) $normal $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
 echo -n -s $status_color $suffix ' ' $normal
