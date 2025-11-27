1a25e331f7f58b6f04d166441c598133287bbfad
crybot <crybot@hotmail.it>
2025-11-27 22:09:06 +0100

    Pin hyprsunset to nixpkgs-unstable to get working profiles

--- nixos-config/flake.nix
+++ nixos-config/flake.nix
@@ -3,6 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     home-manager = {
       url = "github:nix-community/home-manager/master";
@@ -18,52 +20,65 @@
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, home-manager, catppuccin, ... }: {
-    nixosConfigurations = {
-      #NOTE: Use your hostname to name each nixosSystem; nix will look for the system's hostname when running 
-      # nixos rebuild and will choose the right configuration accordingly
+  outputs = inputs@{ self, nixpkgs, nixpkgs-unstable, home-manager, catppuccin, ... }:
+    let 
+      hyprsunsetOverlay = final: prev:
+        let
+          system = "x86_64-linux";
+          unstablePkgs = import nixpkgs-unstable {
+            inherit system;
+            # reuse the same config (allowUnfree, etc.)
+            config = prev.config;
+          };
+        in {
+          hyprsunset = unstablePkgs.hyprsunset;
+        };
+    in {
+      nixosConfigurations = {
+        #NOTE: Use your hostname to name each nixosSystem; nix will look for the system's hostname when running 
+        # nixos rebuild and will choose the right configuration accordingly
 
-      # Define the configuration for each machine
-      nixos-desktop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/desktop/desktop.nix
-          ./configuration.nix
-          catppuccin.nixosModules.catppuccin
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.crybot = {
-              imports = [
-                ./home/default.nix 
-                ./hosts/desktop/home.nix
-                catppuccin.homeModules.catppuccin
-              ];
-            };
+        # Define the configuration for each machine
+        nixos-desktop = nixpkgs.lib.nixosSystem {
+          modules = [
+            { nixpkgs.overlays = [ hyprsunsetOverlay ]; }
+            ./hosts/desktop/desktop.nix
+            ./configuration.nix
+            catppuccin.nixosModules.catppuccin
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.crybot = {
+                imports = [
+                  ./home/default.nix 
+                  ./hosts/desktop/home.nix
+                  catppuccin.homeModules.catppuccin
+                ];
+              };
 
-          }
-        ];
-      };
+            }
+          ];
+        };
 
-      nixos-laptop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./hosts/laptop/laptop.nix
-          ./configuration.nix
-          catppuccin.nixosModules.catppuccin
-          home-manager.nixosModules.home-manager {
-            home-manager.useGlobalPkgs = true;
-            home-manager.useUserPackages = true;
-            home-manager.users.crybot = {
-              imports = [
-                ./home/default.nix 
-                ./hosts/laptop/home.nix
-                catppuccin.homeModules.catppuccin
-              ];
-            };
-          }
-        ];
+        nixos-laptop = nixpkgs.lib.nixosSystem {
+          modules = [
+            { nixpkgs.overlays = [ hyprsunsetOverlay ]; }
+            ./hosts/laptop/laptop.nix
+            ./configuration.nix
+            catppuccin.nixosModules.catppuccin
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.crybot = {
+                imports = [
+                  ./home/default.nix 
+                  ./hosts/laptop/home.nix
+                  catppuccin.homeModules.catppuccin
+                ];
+              };
+            }
+          ];
+        };
       };
     };
-  };
 }
